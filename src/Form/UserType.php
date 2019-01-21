4c50b93e6823a5fb139430029fc168ab6ca81744
Patrick Etcheverry <patrick.etcheverry@iutbayonne.univ-pau.fr>
2019-01-21 09:35:36 +0100

    Sécurisation des données saisies dans le formulaire d'inscription

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,9 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
 
 class UserType extends AbstractType
 {
@@ -15,7 +18,14 @@ class UserType extends AbstractType
             ->add('prenom')
             ->add('nom')
             ->add('email')
-            ->add('password')
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe saisis ne correspondent pas.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Répétez la saisie du mot de passe'],
+            ])
         ;
     }
 
